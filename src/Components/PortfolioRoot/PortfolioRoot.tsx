f8eb7c34d1651cf34c0c24a5c9345bb1bf3fbcb0
Akshar V Patel <aksharpatel144@gmail.com>
2022-10-15 15:34:14 -0400

    removing unused code

--- src/Components/PortfolioRoot/PortfolioRoot.tsx
+++ src/Components/PortfolioRoot/PortfolioRoot.tsx
@@ -8,13 +8,6 @@ const Footer = React.lazy(
   () => import(/* webpackChunkName: "Footer" */ "../Footer/Footer")
 );
 
-function gen(number) {
-  const nums = [];
-  for (let i = 0; i <= number; i++) {
-    nums.push(i);
-  }
-  return nums;
-}
 const PortfolioRoot: React.FC<any> = (props) => {
   return (
     <React.Suspense>
